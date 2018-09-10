31f192f9a63d68028b3df9158f99eaa62fcc8e00
Kevin Mike <kevin_22894@hotmail.com>
2018-09-09 23:55:56 -0500

    firts commit

---
+++ Impuesto.js
@@ -0,0 +1,9 @@
+function botonclick(){
+	alert("Se ha ejecutado la funcion contenida en el boton");
+}
+function calcularImpuesto(){
+	return 13;
+}
+function divclick(caja){
+	alert("Has presionado la"+ caja.id );
+}
